c57415ff3ffdbd411e8a0b027b11a217c674eeed
Ethan Mangosing <emangosing@email.arizona.edu>
2018-04-03 18:36:44 -0700

    blahblah

--- js/game.js
+++ js/game.js
@@ -25,23 +25,23 @@ gameScene.create = function() {
  
   // background
   let bg = this.add.sprite(0, 0, 'onodera');
-
-  game.world.createMultiple(12, 'block', 0, true);
+  let boundaries = this.add.group();
+  boundaries.createMultiple(12, 'block', 0, true);
 
   // change origin to the center of the sprite
   bg.alignIn(this.world.bounds, Phaser.CENTER);
 
     //  Align all of the sprites around the picture
-    game.world.getChildAt(1).alignTo(pic, Phaser.TOP_LEFT);
-    game.world.getChildAt(2).alignTo(pic, Phaser.TOP_CENTER);
-    game.world.getChildAt(3).alignTo(pic, Phaser.TOP_RIGHT);
-    game.world.getChildAt(4).alignTo(pic, Phaser.LEFT_TOP);
-    game.world.getChildAt(5).alignTo(pic, Phaser.LEFT_CENTER);
-    game.world.getChildAt(6).alignTo(pic, Phaser.LEFT_BOTTOM);
-    game.world.getChildAt(7).alignTo(pic, Phaser.RIGHT_TOP);
-    game.world.getChildAt(8).alignTo(pic, Phaser.RIGHT_CENTER);
-    game.world.getChildAt(9).alignTo(pic, Phaser.RIGHT_BOTTOM);
-    game.world.getChildAt(10).alignTo(pic, Phaser.BOTTOM_LEFT);
-    game.world.getChildAt(11).alignTo(pic, Phaser.BOTTOM_CENTER);
-    game.world.getChildAt(12).alignTo(pic, Phaser.BOTTOM_RIGHT);
+    boundaries.getChildAt(1).alignTo(pic, Phaser.TOP_LEFT);
+    boundaries.getChildAt(2).alignTo(pic, Phaser.TOP_CENTER);
+    boundaries.getChildAt(3).alignTo(pic, Phaser.TOP_RIGHT);
+    boundaries.getChildAt(4).alignTo(pic, Phaser.LEFT_TOP);
+    boundaries.getChildAt(5).alignTo(pic, Phaser.LEFT_CENTER);
+    boundaries.getChildAt(6).alignTo(pic, Phaser.LEFT_BOTTOM);
+    boundaries.getChildAt(7).alignTo(pic, Phaser.RIGHT_TOP);
+    boundaries.getChildAt(8).alignTo(pic, Phaser.RIGHT_CENTER);
+    boundaries.getChildAt(9).alignTo(pic, Phaser.RIGHT_BOTTOM);
+    boundaries.getChildAt(10).alignTo(pic, Phaser.BOTTOM_LEFT);
+    boundaries.getChildAt(11).alignTo(pic, Phaser.BOTTOM_CENTER);
+    boundaries.getChildAt(12).alignTo(pic, Phaser.BOTTOM_RIGHT);
 }
