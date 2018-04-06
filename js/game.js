4abdedfbafb7dd39f2ea172f4d34ad30bc3d3655
Ethan Mangosing <emangosing@email.arizona.edu>
2018-04-06 14:29:56 -0700

    boundaries revert to arcade

--- js/game.js
+++ js/game.js
@@ -7,11 +7,11 @@ let config = {
   width: 448, // game width
   height: 704, // game height
   physics: {
-    default: 'matter'
-    // arcade: {
-    //   gravity: {y: 300 },
-    //   debug: false
-    // }
+    default: 'arcade',
+    arcade: {
+      gravity: {y: 300 },
+      debug: false
+    }
   },
   scene: gameScene // our newly created scene
 };
@@ -32,79 +32,72 @@ gameScene.preload = function() {
 // executed once, after assets were loaded
 gameScene.create = function() {
 
-  // // Enable Box2D physics
-  // this.physics.startSystem(Phaser.Physics.BOX2D);
-  //
-  // this.physics.box2d.gravity.y = 500;
-  // this.physics.box2d.setBoundsToWorld();
-  // this.physics.box2d.restitution = 0.5;
-
   // background
   let bg = this.add.sprite(this.sys.game.config.width/2, this.sys.game.config.height/2, 'riko');
   bg.setScale(.5);
   console.log(bg);
-  let boundaryLeft = this.add.staticGroup({
+  let boundaryLeft = this.physics.add.staticGroup({
   	key: 'block',
   	repeat: 20,
     setXY: { x: 16, y: 16, stepY: 32 }
   });
 
-  let boundaryRight = this.add.staticGroup({
+  let boundaryRight = this.physics.add.staticGroup({
   	key: 'block',
   	repeat: 20,
     setXY: { x: this.sys.game.config.width - 16, y: 16, stepY: 32 }
   });
 
-  let boundaryBottom = this.add.staticGroup({
+  let boundaryBottom = this.physics.add.staticGroup({
   	key: 'block',
   	repeat: 13,
     setXY: { x: 16, y: this.sys.game.config.height - 16, stepX: 32 }
   });
 
-  let column1 = this.add.staticGroup({
+  let column1 = this.physics.add.staticGroup({
       key: 'star',
       repeat: 5,
       setXY: {x:80, y:80, stepY: 96}
   });
 
-  let column2 = this.add.staticGroup({
+  let column2 = this.physics.add.staticGroup({
       key: 'star',
       repeat: 5,
       setXY: {x:128, y:128, stepY: 96}
   });
 
-  let column3 = this.add.staticGroup({
+  let column3 = this.physics.add.staticGroup({
       key: 'star',
       repeat: 5,
       setXY: {x: 176, y: 80, stepY: 96}
   });
 
-  let column4 = this.add.staticGroup({
+  let column4 = this.physics.add.staticGroup({
       key: 'star',
       repeat: 5,
       setXY: {x: 224, y: 128, stepY: 96}
   });
 
-  let column5 = this.add.staticGroup({
+  let column5 = this.physics.add.staticGroup({
       key: 'star',
       repeat: 5,
       setXY: {x: 272, y: 80, stepY: 96}
   });
 
-  let column6 = this.add.staticGroup({
+  let column6 = this.physics.add.staticGroup({
       key: 'star',
       repeat: 5,
       setXY: {x: 320, y: 128, stepY: 96}
   });
 
-  let column7 = this.add.staticGroup({
+  let column7 = this.physics.add.staticGroup({
       key: 'star',
       repeat: 5,
       setXY: {x: 368, y: 80, stepY: 96}
   });
 
   //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-  let stars = this.matter.add.group({
+  let stars = this.physics.add.group({
     key: 'star',
     repeat: 5,
     setXY: { x: 40, y: 0, stepX: 60 }
@@ -140,15 +133,15 @@ gameScene.create = function() {
     child.setCircle(15.5);
   });
 
-  this.matter.add.collider(stars, boundaryLeft);
-  this.matter.add.collider(stars, boundaryRight);
-  this.matter.add.collider(stars, boundaryBottom);
-  this.matter.add.collider(stars, column1);
-  this.matter.add.collider(stars, column2);
-  this.matter.add.collider(stars, column3);
-  this.matter.add.collider(stars, column4);
-  this.matter.add.collider(stars, column5);
-  this.matter.add.collider(stars, column6);
-  this.matter.add.collider(stars, column7);
+  this.physics.add.collider(stars, boundaryLeft);
+  this.physics.add.collider(stars, boundaryRight);
+  this.physics.add.collider(stars, boundaryBottom);
+  this.physics.add.collider(stars, column1);
+  this.physics.add.collider(stars, column2);
+  this.physics.add.collider(stars, column3);
+  this.physics.add.collider(stars, column4);
+  this.physics.add.collider(stars, column5);
+  this.physics.add.collider(stars, column6);
+  this.physics.add.collider(stars, column7);
 
 }
